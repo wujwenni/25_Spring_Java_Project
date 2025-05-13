20ab351e4ada683780d56d2bb54fa6212a9dbcda
wujwenni <woojin2754@naver.com>
2025-05-13 16:58:54 +0900

    test3

--- Test_P/src/Test.java
+++ Test_P/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("코리안특급 손흥민 화이팅");
+		System.out.println("코리안특급 손흥민 화이팅");
+		
 	}
 
 }
